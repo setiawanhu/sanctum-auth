c5e26833a8cbd27a9ebe8abf609d9b379565ffb0
Setiawan Hu <setiawanhu@gmail.com>
2020-05-31 02:08:45 +0700

    Updated documentations

--- src/HasRole.php
+++ src/HasRole.php
@@ -5,6 +5,14 @@ namespace Hu\Auth;
 
 use Hu\Auth\Models\Role;
 
+/**
+ * Trait HasRole
+ *
+ * @package Hu\Auth
+ *
+ * @property int role_id
+ * @property Role role
+ */
 trait HasRole
 {
     public function role()
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -4,6 +4,16 @@ namespace Hu\Auth\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Role
+ *
+ * @package Hu\Auth\Models
+ *
+ * @property int id
+ * @property string name
+ * @property string created_at
+ * @property string updated_at
+ */
 class Role extends Model
 {
     protected $fillable = [
